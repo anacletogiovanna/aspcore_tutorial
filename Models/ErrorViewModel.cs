aaa6275c72f2b6dad1e01c839adbd1cf8d767a41
Giovanna Anacleto <giovanna18batalha@gmail.com>
2020-02-24 17:17:07 -0300

    Create aspcore files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspcore_tutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
